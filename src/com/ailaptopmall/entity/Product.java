cfd8d9562c3cd2a1ff54165de89df72c8da6a7df
Syuan <clou40308@gmail.com>
2024-09-13 17:49:34 +0800

    2024/09/13:修改product

--- src/com/ailaptopmall/entity/Product.java
+++ src/com/ailaptopmall/entity/Product.java
@@ -1,5 +1,22 @@
 package com.ailaptopmall.entity;
 
+import java.time.LocalDate;
+
 public class Product {
 
+	private int id; // PKey(equals + hashCode), AUTO-INCREMENT
+
+	private String name; // unique, required, 1~60個字元
+
+	private double unitPrice; // required, >0 , 定價(售價)
+
+	private int stock; // required，負數
+
+	private String photoUrl; // optional
+
+	private String category; // 總類, required, null,""~250個字元
+
+	private String description = ""; // optional, 0~250個字元
+
+	private LocalDate releaseDate; // required
 }
